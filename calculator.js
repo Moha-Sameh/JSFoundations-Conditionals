24e18441d7216d7db54908c63e9e6824967046ef
@Moha-Sameh <ms.slama01@hotmail.com>
2021-02-02 17:28:24 +0200

    We are doing fine

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
 // Define the numbers and the operation here
+let firstnumber = 10;
+let secondnumber = 20;
+let operation = "Addition";
 
 // Write if-statements here
+
+if (operation === "Substraction") {
+  console.log("This operation is false");
+} else if (operation === "Addition") {
+  console.log(firstnumber + secondnumber);
+} else if (operation === "Multiplication") {
+  console.log("No, nothing here");
+} else {
+  console.log("Go back and study");
+}
+
+console.log("Task done successfully");
